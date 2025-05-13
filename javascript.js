1c9b3a03077bae37e4d497fbd0934657b7f601b2
bigmaniac <1matik.m@gmail.com>
2025-05-13 22:12:00 +0200

    Add div coloring and create grid button
    - add function to random color div in the grid on mouseover
    - add button to create a new grid

--- javascript.js
+++ javascript.js
@@ -3,9 +3,21 @@ document.body.setAttribute(
 
 const title = document.createElement("h1");
 title.classList.add("title");
-title.setAttribute("style", "font-family: 'Brush Script MT'");
+title.setAttribute("style", `
+    font-family: 'Brush Script MT';
+    margin: 20px 0 0 0;
+    `);
 title.textContent = "Etch A Sketch";
 document.body.appendChild(title);
+
+const button = document.createElement("button");
+button.classList.add("button");
+button.setAttribute("style", `
+    margin: 20px 0;
+    padding: 8px 16px;
+    `)
+button.textContent = "Create a grid";
+document.body.appendChild(button);
 
 const container = document.createElement("div");
 container.classList.add("container");
@@ -20,23 +32,29 @@ container.setAttribute(
     border-width: 10px;
     display: flex;
     flex-wrap: wrap;
-    flex-grow: 1;
     `);
 document.body.appendChild(container);
 
-const blocks = 16
-const columns = Math.ceil(Math.sqrt(blocks));
-const rows = Math.ceil(blocks / columns);
-const blockWidth = 960 / columns;
-const blockHeight = 960 / rows;
+const blocksPerSide = 16
+const blocks = blocksPerSide * blocksPerSide
+const size = 960 / blocksPerSide
 
 for (let i = 0; i < blocks; i++) {
     const grid = document.createElement("div");
     grid.classList.add("grid");
     grid.setAttribute("style", `
-        background-color: pink;
-        width: ${blockWidth}px;
-        height: ${blockHeight}px;
+        width: ${size}px;
+        height: ${size}px;
         `);
     container.appendChild(grid);
 }
+
+const colorDiv = document.querySelectorAll(".grid");
+colorDiv.forEach(function(div) {
+    div.addEventListener("mouseover", function() {
+        let randomR = Math.floor(Math.random() * 255);
+        let randomG = Math.floor(Math.random() * 255);
+        let randomB = Math.floor(Math.random() * 255);
+        div.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
+    })
+})
