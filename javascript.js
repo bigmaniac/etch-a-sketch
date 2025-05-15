f258a49dcc6651d2c63ce1f68739b6a0810dd11f
bigmaniac <1matik.m@gmail.com>
2025-05-15 23:01:08 +0200

    Modify javascript.js
    - create function to color divs
    - create function to create grid
    - create function to remove grid
    - modify event listener to create new grid

--- javascript.js
+++ javascript.js
@@ -16,8 +16,10 @@ button.setAttribute("style", `
     margin: 20px 0;
     padding: 8px 16px;
     `)
-button.textContent = "Create a grid";
+button.textContent = "Create new grid";
 document.body.appendChild(button);
+
+let blocksPerSide = 16;
 
 const container = document.createElement("div");
 container.classList.add("container");
@@ -32,29 +34,74 @@ container.setAttribute(
     border-width: 10px;
     display: flex;
     flex-wrap: wrap;
+    background-color: black;
+    opacity: 1
     `);
 document.body.appendChild(container);
 
-const blocksPerSide = 16
-const blocks = blocksPerSide * blocksPerSide
-const size = 960 / blocksPerSide
-
-for (let i = 0; i < blocks; i++) {
-    const grid = document.createElement("div");
-    grid.classList.add("grid");
-    grid.setAttribute("style", `
-        width: ${size}px;
-        height: ${size}px;
-        `);
-    container.appendChild(grid);
+function colorDivs() {
+    const colorDiv = document.querySelectorAll(".grid");
+    colorDiv.forEach(function(div) {
+        div.addEventListener("mouseover", function() {
+            let randomR = Math.floor(Math.random() * 255);
+            let randomG = Math.floor(Math.random() * 255);
+            let randomB = Math.floor(Math.random() * 255);
+            let checkColor = window.getComputedStyle(div).backgroundColor;
+            if (checkColor === "rgb(192, 192, 192)") {
+                div.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
+            } else {
+                let checkOpacity = parseFloat(window.getComputedStyle(div).opacity);
+                if (checkOpacity <= 1 && checkOpacity > 0) {
+                    div.style.opacity = checkOpacity - 0.1
+                }
+            }
+        })
+    })
 }
 
-const colorDiv = document.querySelectorAll(".grid");
-colorDiv.forEach(function(div) {
-    div.addEventListener("mouseover", function() {
-        let randomR = Math.floor(Math.random() * 255);
-        let randomG = Math.floor(Math.random() * 255);
-        let randomB = Math.floor(Math.random() * 255);
-        div.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
-    })
+function createGrid(numberOfBlocksPerSide) {
+    numberOfBlocksPerSide = blocksPerSide * blocksPerSide
+    const size = 960 / blocksPerSide
+    for (let i = 0; i < numberOfBlocksPerSide; i++) {
+        const grid = document.createElement("div");
+        grid.classList.add("grid");
+        grid.setAttribute("style", `
+            width: ${size}px;
+            height: ${size}px;
+            background-color: rgb(192, 192, 192)
+            `);
+        container.appendChild(grid);
+    }
+    colorDivs();
+}
+
+createGrid(blocksPerSide);
+
+function removeGrid() {
+    const element = document.querySelector(".container");
+    while (element.firstChild) {
+        element.removeChild(element.firstChild);
+    };
+};
+
+button.addEventListener("click", () => {
+    // do {
+    //     blocksPerSide = parseInt(prompt("Enter number of blocks per side between 1 - 64."));
+    // } while (isNaN(blocksPerSide) || blocksPerSide < 1 || blocksPerSide > 64);
+
+    while (true) {
+        blocksPerSide = parseInt(prompt("Enter number of blocks per side between 1 - 64."));
+        if (isNaN(blocksPerSide)) {
+            return
+        }
+        else if (blocksPerSide > 0 && blocksPerSide < 65) {
+            break
+        }
+        alert("Please enter a valid value.")
+    }
+
+
+    // blocksPerSide = parseInt(prompt("Enter number of blocks per side between 1 - 100."));
+    removeGrid();
+    createGrid(blocksPerSide);
 })
